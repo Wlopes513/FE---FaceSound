10dfca1fbaddf8ffcdeca153e5638057d4bb3d0f
BebadoDoBosque <gabriel.fer.c@hotmail.com>
2023-11-14 15:51:13 -0300

    começo do visual da tabelas user e people

--- src/views/pages/people.js
+++ src/views/pages/people.js
@@ -26,45 +26,47 @@ class People extends Component {
                 </CHeaderNav>
                 <div className="app-body">
                     <div className='container-centered'>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nome</th>
-                                    <th>CPF</th>
-                                    <th>Telefone</th>
-                                    <th>Andar</th>
-                                    <th>Criado</th>
-                                    <th>Modificado</th>
-                                    <th>Ações</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>John Doe</td>
-                                    <td>123.456.789-01</td>
-                                    <td>(123) 456-7890</td>
-                                    <td>3</td>
-                                    <td>2023-11-12 10:30:45</td>
-                                    <td>2023-11-12 14:20:30</td>
-                                    <td>
-                                        <button
-                                            className="btn btn-warning btn-sm"
-                                            type='button'
-                                            onClick={this.handleTest}>
-                                            Editar
-                                        </button>
-                                        <button
-                                            className="btn btn-danger btn-sm"
-                                            type='button'
-                                            onClick={this.handleTest}>
-                                            Excluir
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div className='table-content'>
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nome</th>
+                                        <th>CPF</th>
+                                        <th>Telefone</th>
+                                        <th>Andar</th>
+                                        <th>Criado</th>
+                                        <th>Modificado</th>
+                                        <th>Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>John Doe</td>
+                                        <td>123.456.789-01</td>
+                                        <td>(123) 456-7890</td>
+                                        <td>3</td>
+                                        <td>2023-11-12 10:30:45</td>
+                                        <td>2023-11-12 14:20:30</td>
+                                        <td>
+                                            <button
+                                                className="btn btn-warning btn-sm"
+                                                type='button'
+                                                onClick={this.handleTest}>
+                                                Editar
+                                            </button>
+                                            <button
+                                                className="btn btn-danger btn-sm"
+                                                type='button'
+                                                onClick={this.handleTest}>
+                                                Excluir
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
