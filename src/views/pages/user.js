5b7905a7b963425fa3679fe140cb47e6e0ac4506
BebadoDoBosque <gabriel.fer.c@hotmail.com>
2023-11-14 22:06:36 -0300

    cadastrar user

--- src/views/pages/user.js
+++ src/views/pages/user.js
@@ -3,7 +3,9 @@ import {
     CHeaderNav,
 } from '@coreui/react';
 import { withTranslation } from 'react-i18next';
+import { Button, Card, CardBody, Col, Row } from 'reactstrap';
 import Header from '../../containers/defaultHeader';
+//import { ModalRegisterComponent } from '../../components/modals';
 
 class User extends Component {
     constructor(props) {
@@ -12,10 +14,12 @@ class User extends Component {
         this.handleTest = this.handleTest.bind(this);
     }
 
-    handleTest(event) {
+    handleModal(event) {
         event.preventDefault();
 
-        alert("O gabriel é muito feio");
+        const { IsOpen } = this.state;
+
+        this.setState({ IsOpen: !IsOpen });
     }
 
     render() {
@@ -26,45 +30,52 @@ class User extends Component {
                 </CHeaderNav>
                 <div className="app-body">
                     <div className='container-centered'>
-                        <div className='table-content'>
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>Nome</th>
-                                        <th>Email</th>
-                                        <th>Admin</th>
-                                        <th>Criado</th>
-                                        <th>Modificado</th>
-                                        <th>Ações</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>John Doe</td>
-                                        <td>teste@gmail.com</td>
-                                        <td>Jonas</td>
-                                        <td>2023/11/12</td>
-                                        <td>2023/11/12</td>
-                                        <td>
-                                            <button
-                                                className="btn btn-warning btn-sm btn-visual"
-                                                type='button'
-                                                onClick={this.handleTest}>
-                                                Editar
-                                            </button>
-                                            <button
-                                                className="btn btn-danger btn-sm btn-visual"
-                                                type='button'
-                                                onClick={this.handleTest}>
-                                                Excluir
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                        <Card>
+                            <CardBody>
+                                <Row className='mb-4'>
+                                    <Col className='d-flex justify-content-end'>
+                                        <Button type="button" color='primary' onClick={this.handleModal} id="ButtonRegister" name="ButtonRegister">Cadastrar</Button>
+                                    </Col>
+                                </Row>
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nome</th>
+                                            <th>Email</th>
+                                            <th>Admin</th>
+                                            <th>Criado</th>
+                                            <th>Modificado</th>
+                                            <th>Ações</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>John Doe</td>
+                                            <td>teste@gmail.com</td>
+                                            <td>Jonas</td>
+                                            <td>2023/11/12</td>
+                                            <td>2023/11/12</td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-warning btn-sm btn-visual"
+                                                    type='button'
+                                                    onClick={this.handleTest}>
+                                                    Editar
+                                                </button>
+                                                <button
+                                                    className="btn btn-danger btn-sm btn-visual"
+                                                    type='button'
+                                                    onClick={this.handleTest}>
+                                                    Excluir
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </CardBody>
+                        </Card>
                     </div>
                 </div>
             </div>
