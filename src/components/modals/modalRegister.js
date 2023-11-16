3906da8d3e8e0adcc6852bd19c1ffcbbf4258172
BebadoDoBosque <gabriel.fer.c@hotmail.com>
2023-11-16 20:13:06 -0300

    api cadastrar

--- src/components/modals/modalRegister.js
+++ src/components/modals/modalRegister.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
 import { Button, Col, FormGroup, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader, Row } from 'reactstrap';
-import ImageUpload from './ImageUpload';
+import ImageUpload from './imageUpload';
 
 class ModalRegister extends Component {
     constructor(props) {
         super(props)
         this.state = { Name: "", CPF: "", Telefone: "", Andar: "" };
     }
+
+    handleRegister = async () => {
+        const { Name, CPF, Telefone, Andar } = this.state;
+        const { toggle } = this.props;
+
+        try {
+            const response = await fetch('https://35.199.105.38:5005/api/v1/persons', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name:Name,
+                    cpf:CPF,
+                    phone:Telefone,
+                    floor:Andar
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Erro na requisição: ${response.status}`);
+            }
+
+            console.log('Cadastro bem-sucedido!');
+            toggle();
+        } catch (error) {
+            console.error('Erro no cadastro:', error.message);
+        }
+    };
 
     render() {
         const { CPF, Name, Telefone, Andar } = this.state;
@@ -52,7 +81,7 @@ class ModalRegister extends Component {
                     <ImageUpload />
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={() => { }}>
+                    <Button color="primary" onClick={this.handleRegister}>
                         Cadastrar
                     </Button>{' '}
                     <Button color="secondary" onClick={toggle}>
